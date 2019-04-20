69be194299ad272f0b1695e51debb6400d6a4e6a
Your-Full-Name <your-email-address>
2019-04-20 14:14:34 -0400

    classes objects , inheritance

---
+++ Employee.py
@@ -0,0 +1,23 @@
+class Employee: 
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay 
+        self.email = first + '.' + last + '@company.com'
+    
+    def fullname(self):
+        return '{} {}'.format(self.first , self.last) 
+
+
+emp_1 = Employee('Xavier', 'Carty' , 100000)
+emp_2 = Employee('Test' , 'User' , 100000)
+
+
+print(Employee.fullname(emp_1))
+print(emp_1.fullname())
+
+
+print(emp_1.email)
+print(emp_2.email)
+
+print(emp_1.fullname())
